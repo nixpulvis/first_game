5fe185e927f76bbb3520b7076e0efe1988ba8730
Ryan Brown <brown.ry@husky.neu.edu>
2013-10-12 11:42:28 -0400

    Commented code!

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Initializing http server
 var port = 1337;
 
 var app = require('http').createServer(handler),
@@ -21,39 +22,52 @@ function handler(req, res) {
   });
 }
 
-//
+// Delay in ms before we send another update to connected players
 var updateRate = 25;
 
 // Array of Players connected to server
 var players = [];
 
-// a KeyEvent is a {Number : Boolean}
-// {key : pressed}
-
+// True if a new cookie was spawned this tick, false otherwise.
+// Determines whether or not to send a new cookie packet to players
+// This avoids spamming cookie packets when the position doesn't change
 var newCookie = false;
 
-// a Cookie is delicious
+// a Cookie is an object with:
+// x (Number), y (Number)
 function Cookie () {
   this.x = 0;
   this.y = 0;
 
+  // Spawn a new cookie on screen
   this.spawn = function() {
     this.x = Math.random()*(500-32);
     this.y = Math.random()*(500-32);
+
+    // If new cookie is colliding with player, generate a new one!
+    for(var i = 0; i < players.length; i++) {
+      if(this.collide(players[i])) {
+        this.spawn();
+        return;
+      }
+    }
   }
 
+  // Bounding box collision with player
   this.collide = function(player) {
     return (this.x <= player.x+32 && this.x+32 >= player.x &&
       this.y <= player.y+32 && this.y+32 >= player.y);
   }
 }
 
+// Initialize cookie for game
 var cookie = new Cookie();
 cookie.spawn();
 
 // a Player is an object with:
-// - x (Number), y (Number), id (Unique Number),
-// - keys (Array of Number)
+// - x (Number), y (Number), id (Unique String),
+// - keys (Array of Number), score (Number), speed (Number)
+// - color (String, form:"rgb(r,g,b)" with r,g,b being Number)
 function Player (id) {
   this.id = id;
   this.x = 0;
@@ -62,6 +76,7 @@ function Player (id) {
 
   this.speed = 5;
 
+  // Randomly generate color
   this.color = "rgb("+
     Math.floor(Math.random()*255)+","+
     Math.floor(Math.random()*255)+","+
@@ -70,6 +85,7 @@ function Player (id) {
   // Keys currently down by this player
   this.keys = {};
 
+  // Moves player depending on keys down and keeps them on screen
   this.move = function() {
     // 37 === LEFT
     // 38 === UP
@@ -96,7 +112,7 @@ function Player (id) {
   }
 }
 
-// update the game
+// Update the game, move players, check for collision
 function update() {
   for(var i = 0; i < players.length; i++){
     players[i].move();
@@ -112,17 +128,20 @@ function update() {
   }
 }
 
-
+// On a new connection with given Socket
 io.sockets.on('connection', function (socket) {
+  // Log the new player's ID
   console.log("New Player: "+socket.id);
 
+  // Generate a Player object from sockets id and add to players array
   var player = new Player(socket.id);
   players.push(player);
 
+  // Emit the player's ID and cookie location
   socket.emit('registration', {id : player.id});
   socket.emit('cookie', {cookie : cookie});
 
-  // KeyEvent ->
+  // If key pressed down, add to player's keys array, otherwise remove it
   socket.on('keyEvent', function (data) {
     for (var keyCode in data) {
       if (data[keyCode]) {
@@ -133,6 +152,7 @@ io.sockets.on('connection', function (socket) {
     }
   });
 
+  // Request to change color, set player's color to given color
   socket.on('changeColor', function (data) {
     for(var i = 0; i < players.length; i++) {
       if(players[i].id == socket.id) {
@@ -142,7 +162,8 @@ io.sockets.on('connection', function (socket) {
     }
   });
 
-  socket.on('disconnect', function(data) {
+  // Player has disconnected, remove him from array of players
+  socket.on('disconnect', function (data) {
     console.log("Player Disconnected: "+socket.id);
     for(var i = 0; i < players.length; i++) {
       if(players[i].id == socket.id) {
@@ -154,14 +175,18 @@ io.sockets.on('connection', function (socket) {
 
 });
 
+// Game loop. Run every "updateRate" ms
 setInterval(function() {
+  // Update game
   update();
 
+  // If we have a new cookie, emit information to everyone
   if(newCookie) {
     io.sockets.emit('cookie', {cookie : cookie});
     newCookie = false;
   }
 
+  // Send array of player to everyone
   io.sockets.emit('update', {players : players});
 }, updateRate);
 
