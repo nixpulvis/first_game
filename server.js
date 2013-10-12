d7ecbb0edd8f24bc6bfa21bd8b81176e8684d636
Ryan Brown <brown.ry@husky.neu.edu>
2013-10-11 20:03:48 -0400

    Added cookies, changed id system

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ var app = require('http').createServer(handler),
     io = require('socket.io').listen(app),
     fs = require('fs')
 
-app.listen(port, '10.0.0.7');
+io.set('log level', 1);
+
+app.listen(port, '10.0.0.2');
 
 function handler(req, res) {
   fs.readFile(__dirname + '/index.html',
@@ -20,10 +22,7 @@ function handler(req, res) {
 }
 
 //
-var updateRate = 50;
-
-//
-var nextID = 0;
+var updateRate = 25;
 
 // Array of Players connected to server
 var players = [];
@@ -31,14 +30,37 @@ var players = [];
 // a KeyEvent is a {Number : Boolean}
 // {key : pressed}
 
+var newCookie = false;
+
+// a Cookie is delicious
+function Cookie () {
+  this.x = 0;
+  this.y = 0;
+
+  this.spawn = function() {
+    this.x = Math.random()*(500-32);
+    this.y = Math.random()*(500-32);
+  }
+
+  this.collide = function(player) {
+    return (this.x <= player.x+32 && this.x+32 >= player.x &&
+      this.y <= player.y+32 && this.y+32 >= player.y);
+  }
+}
+
+var cookie = new Cookie();
+cookie.spawn();
 
 // a Player is an object with:
 // - x (Number), y (Number), id (Unique Number),
 // - keys (Array of Number)
-function Player () {
-  this.id = nextID++;
+function Player (id) {
+  this.id = id;
   this.x = 0;
   this.y = 0;
+  this.score = 0;
+
+  this.speed = 5;
 
   // Keys currently down by this player
   this.keys = {};
@@ -49,17 +71,23 @@ function Player () {
     // 39 === RIGHT
     // 40 === DOWN
     if (37 in this.keys) {
-      this.x -= 10;
+      this.x -= this.speed;
     }
     if (38 in this.keys) {
-      this.y -= 10;
+      this.y -= this.speed;
     }
     if (39 in this.keys) {
-      this.x += 10;
+      this.x += this.speed;
     }
     if (40 in this.keys) {
-      this.y += 10;
+      this.y += this.speed;
     }
+
+    if(this.x < -32) this.x = 500;
+    if(this.x > 500) this.x = -32;
+
+    if(this.y < -32) this.y = 500;
+    if(this.y > 500) this.y = -32;
   }
 }
 
@@ -68,14 +96,26 @@ function update() {
   for(var i = 0; i < players.length; i++){
     players[i].move();
   }
+
+  for(var i = 0; i < players.length; i++){
+    if(cookie.collide(players[i])) {
+      players[i].score++;
+      cookie.spawn();
+      newCookie = true;
+      break;
+    }
+  }
 }
 
 
 io.sockets.on('connection', function (socket) {
-  var player = new Player();
+  console.log("New Player: "+socket.id);
+
+  var player = new Player(socket.id);
   players.push(player);
 
   socket.emit('registration', {id : player.id});
+  socket.emit('cookie', {cookie : cookie});
 
   // KeyEvent ->
   socket.on('keyEvent', function (data) {
@@ -86,12 +126,27 @@ io.sockets.on('connection', function (socket) {
         delete player.keys[keyCode];
       }
     }
-  })
+  });
+
+  socket.on('disconnect', function(data) {
+    console.log("Player Disconnected: "+socket.id);
+    for(var i = 0; i < players.length; i++) {
+      if(players[i].id == socket.id) {
+        players.splice(i, 1);
+        break;
+      }
+    }
+  });
 
 });
 
 setInterval(function() {
   update();
+
+  if(newCookie) {
+    io.sockets.emit('cookie', {cookie : cookie});
+    newCookie = false;
+  }
 
   io.sockets.emit('update', {players : players});
 }, updateRate);
